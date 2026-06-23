361eea3a874fc46eb3b6a5aab4f2d09214893601
amiraly26 <zaynah.hcrcs@gmail.com>
2026-06-23 09:43:47 +0400

    Add articles and author collections

---
+++ src/collections/Articles/config.ts
@@ -0,0 +1,84 @@
+import { convertLexicalToPlaintext } from '@payloadcms/richtext-lexical/plaintext'
+import type { CollectionConfig } from 'payload'
+
+import { generateContentSummaryHook } from './hooks/generate-content-summary.hook'
+import { generateSlugHook } from './hooks/generate-slug.hook'
+
+export const Articles: CollectionConfig = {
+  slug: 'articles',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      required: true,
+      unique: true,
+      hooks: { beforeValidate: [generateSlugHook] },
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+    },
+    {
+      name: 'contentSummary',
+      type: 'textarea',
+      required: true,
+      hooks: { beforeValidate: [generateContentSummaryHook] },
+    },
+    {
+      name: 'readTimeInMins',
+      type: 'number',
+      defaultValue: 0,
+      hooks: {
+        beforeChange: [
+          ({ siblingData }) => {
+            delete siblingData.readTimeInMins
+          },
+        ],
+        afterRead: [
+          ({ data }) => {
+            const text = convertLexicalToPlaintext({ data: data?.content })
+            const wordsPerMinute = 200
+            const words = text.trim().split(/\s+/).length
+
+            return Math.max(1, Math.ceil(words / wordsPerMinute))
+          },
+        ],
+      },
+    },
+    {
+      name: 'coverImage',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'author',
+      type: 'relationship',
+      relationTo: 'article-authors',
+      required: true,
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: ['Draft', 'Published'],
+      required: true,
+      defaultValue: 'Draft',
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+      required: true,
+      admin: {
+        condition: (data) => data?.status === 'Published',
+        date: { pickerAppearance: 'dayAndTime' },
+      },
+    },
+  ],
+}
